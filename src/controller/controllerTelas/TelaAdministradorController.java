1e6911e99df3727f51aef403cc385381b01cfcaa
CamilleRc <camillecosta123@gmail.com>
2023-08-28 21:42:52 -0300

    Adicionei telas

---
+++ src/controller/controllerTelas/TelaAdministradorController.java
@@ -0,0 +1,28 @@
+package controller.controllerTelas;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.BorderPane;
+
+public class TelaAdministradorController {
+
+    @FXML
+    private BorderPane fundo;
+
+    @FXML
+    private Button botaoAdicionar;
+
+    @FXML
+    private ImageView botaoVisualizar;
+
+    @FXML
+    private ImageView botaoConta;
+
+    @FXML
+    void botaoVoltar(ActionEvent event) {
+
+    }
+
+}
