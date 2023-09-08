1021ea111a73d4f7c1cbcd9b308ae4696313f8a8
azvmari <202111263@uesb.edu.br>
2023-09-08 00:20:12 -0300

    alterei a tela do admin (falta ajeitar o controller), criei a tela das receitas cadastradas e a das receitas avaliadas (sem controller ainda). falta mt coisa mas por hoje to encerrando

---
+++ src/controller/controllerTelas/TelaReceitasCadastradasController.java
@@ -0,0 +1,124 @@
+package controller.controllerTelas;
+
+import model.Principal;
+import model.Receita;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.ResourceBundle;
+
+import controller.ReceitasSelecionadas;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.control.ScrollPane.ScrollBarPolicy;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+
+public class TelaReceitasCadastradasController implements Initializable {
+  @FXML
+  private VBox listaReceitas;
+
+  @FXML
+  private ScrollPane scroll;
+
+  @FXML
+  public void botaoVoltar() {
+    try {
+      Principal.root = FXMLLoader.load(getClass().getResource("/view/" + Principal.telaMenuAnterior + ".fxml"));
+      Scene scene = new Scene(Principal.root);
+      Principal.palco.setScene(scene);
+    } catch (Exception e) {
+    }
+  }
+
+  @FXML
+  public void filtraTodas() {
+  }
+
+  @FXML
+  public void filtraCafeDaManha() {
+  }
+
+  @FXML
+  public void filtraAlmocoJantar() {
+  }
+
+  @FXML
+  public void filtraSobremesa() {
+  }
+
+  @Override
+  public void initialize(URL url, ResourceBundle rb) {
+    preencherLista();
+  }
+
+  private void preencherLista() {
+    listaReceitas.getChildren().clear();
+
+    for (Receita receita : ReceitasSelecionadas.receitasPossiveis()) {
+      HBox botaoReceita = new HBox();
+      HBox hbox = new HBox();
+      VBox vbox = new VBox();
+      Label nomeReceita = new Label();
+      Label avaliacao = new Label();
+      Label ingredientes = new Label();
+
+      Button editar = new Button();
+      Button excluir = new Button();
+      HBox botoesReceita = new HBox();
+
+      ImageView iconeEditar = new ImageView(new Image("/view/images/icon-editar.png"));
+      ImageView iconeExcluir = new ImageView(new Image("/view/images/icon-lixeira.png"));
+
+      nomeReceita.setText(receita.getNome() + " ");
+      avaliacao.setText(receita.mediaAvaliacao() + " ★");
+
+      String ingredientesTexto = "INGREDIENTES:\n";
+      for (String ingrediente : receita.getIngredientes()) {
+        ingredientesTexto += "• " + ingrediente + "\n";
+      }
+      ingredientes.setText(ingredientesTexto);
+
+      botaoReceita.getStyleClass().setAll("botaoReceita");
+      vbox.getStyleClass().setAll("vbox");
+      hbox.getStyleClass().setAll("hbox");
+      editar.getStyleClass().setAll("botoesReceitas");
+      excluir.getStyleClass().setAll("botoesReceitas");
+      botoesReceita.setSpacing(10);
+      iconeEditar.setFitHeight(28);
+      iconeEditar.setFitWidth(28);
+      iconeExcluir.setFitHeight(28);
+      iconeExcluir.setFitWidth(28);
+      nomeReceita.getStyleClass().setAll("nomeReceita");
+      ingredientes.getStyleClass().setAll("ingredientes");
+
+      botaoReceita.setOnMouseClicked(event -> {
+        try {
+          TelaReceitaEscolhidaController.receitaEscolhida = receita;
+          Principal.telaAnterior = "TelaReceitas";
+
+          Principal.root = FXMLLoader.load(getClass().getResource("/view/TelaReceitaEscolhida.fxml"));
+          Scene scene = new Scene(Principal.root);
+          Principal.palco.setScene(scene);
+        } catch (Exception e) {
+          System.out.print(e);
+        }
+      });
+
+      editar.setGraphic(iconeEditar);
+      excluir.setGraphic(iconeExcluir);
+      botoesReceita.getChildren().setAll(editar, excluir);
+      hbox.getChildren().setAll(nomeReceita, botoesReceita);
+      vbox.getChildren().setAll(hbox, ingredientes);
+      botaoReceita.getChildren().setAll(vbox);
+      listaReceitas.getChildren().add(botaoReceita);
+    }
+  }
+
+}
